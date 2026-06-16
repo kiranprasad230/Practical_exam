efe505b2fe960185de8c24507d54e93cd8541df1
kiranprasad230 <kiranprasad2306@gmail.com>
2026-06-16 13:31:54 +0530

    Add files via upload

---
+++ Aptitude.php
@@ -0,0 +1,10 @@
+1. Speed=60/45=4/3 per km
+Total speed= 60*4/3 = 80km/h
+
+2. The original price of item will be 500 rupees
+
+3. 2,6,12,20,30.42
+
+4. GMPXFS
+
+5. A's present will be 24 years
---
+++ script.js
@@ -0,0 +1,13 @@
+const students = [
+  { name: 'Aarav', marks: 78, passed: true, subject: 'Math' },
+  { name: 'Diya', marks: 42, passed: false, subject: 'Science' },
+  { name: 'Kabir', marks: 88, passed: true, subject: 'Math' },
+  { name: 'Meera', marks: 65, passed: true, subject: 'English' },
+  { name: 'Rohan', marks: 38, passed: false, subject: 'Science' },
+  { name: 'Isha', marks: 92, passed: true, subject: 'Math' },
+  { name: 'Vivaan', marks: 75, passed: true, subject: 'English' },
+  { name: 'Anaya', marks: 70, passed: true, subject: 'Science' }
+]
+const filteredStudents = students.filter(student => student.passed && student.marks >= 70);
+
+const totalMarks = filteredStudents.reduce((sum, student) => sum + student.marks, 0);
